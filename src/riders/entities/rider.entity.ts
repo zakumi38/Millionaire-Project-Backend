267f2d0b4692672e8e189809cc82cb1076f0f0a6
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-09-10 20:55:25 +0630

    Fixed: rider's previousPayment entity

--- src/riders/entities/rider.entity.ts
+++ src/riders/entities/rider.entity.ts
@@ -1,26 +1,28 @@
 import { Column, Entity } from "typeorm";
 import { Common } from "../../common/entities/common.entity";
 
+interface previousPayments {
+    date: string;
+    amount: number;
+}
+
 @Entity()
 export class Rider extends Common {
-  @Column("varchar")
-  name: string;
+    @Column("varchar")
+    name: string
 
-  @Column("varchar", { nullable: true, unique: true })
-  email: string;
+    @Column("varchar", { nullable: true, unique: true })
+    email: string
 
-  @Column("varchar", { unique: true })
-  phoneNumber: string;
+    @Column("varchar", { unique: true })
+    phoneNumber: string
 
-  @Column("float", { nullable: true })
-  overallTotalPayment: number;
+    @Column("float", { nullable: true })
+    overallTotalPayment: number
 
-  @Column("float", { nullable: true })
-  todayIncome: number;
+    @Column("float", { nullable: true })
+    todayIncome: number
 
-  @Column("json", { nullable: true })
-  previousPayments: {
-    date: string,
-    amount: number
-  };
+    @Column("array", { nullable: true })
+    previousPayments: previousPayments[]
 }

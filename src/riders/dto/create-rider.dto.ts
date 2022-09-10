174b3a34242c18734fb4b5d972c28ddb9fe3e67d
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-09-10 20:13:05 +0630

    Added: CRUD for riders

--- src/riders/dto/create-rider.dto.ts
+++ src/riders/dto/create-rider.dto.ts
@@ -1,2 +1,16 @@
 export class CreateRiderDto {
+  readonly name: string;
+
+  readonly email: string;
+
+  readonly phoneNumber: string;
+
+  readonly overallTotalPayment?: number;
+
+  readonly todayIncome?: number;
+
+  readonly previousPayments?: {
+    date: string
+    amount: number
+  };
 }
--- src/riders/riders.service.ts
+++ src/riders/riders.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateRiderDto } from "./dto/create-rider.dto";
 import { UpdateRiderDto } from "./dto/update-rider.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Rider } from "./entities/rider.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class RidersService {
+  constructor(
+    @InjectRepository(Rider)
+    private readonly riderRepository: Repository<Rider>
+  ) {
+  }
+
   create(createRiderDto: CreateRiderDto) {
-    return "This action adds a new rider";
+    const newRider = this.riderRepository.create({ ...createRiderDto });
+    return this.riderRepository.save(newRider);
   }
 
   findAll() {
-    return `This action returns all riders`;
+    return this.riderRepository.find({ order: { id: "ASC" } });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} rider`;
+  async findOne(id: number) {
+    const rider = await this.riderRepository.findOne({ where: { id } });
+    if (!rider) throw new NotFoundException(`Rider #${id} cannot be found`);
+    return rider;
   }
 
-  update(id: number, updateRiderDto: UpdateRiderDto) {
-    return `This action updates a #${id} rider`;
+  async update(id: number, updateRiderDto: UpdateRiderDto) {
+    const rider = await this.riderRepository.preload({
+      id,
+      ...updateRiderDto
+    });
+    if (!rider)
+      throw new NotFoundException(
+        `Rider #${id} cannot be updated because it doesn't exist`
+      );
+    return this.riderRepository.save(rider);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} rider`;
+  async remove(id: number) {
+    const rider = await this.riderRepository.findOneOrFail({
+      where: { id }
+    });
+    if (rider)
+      throw new NotFoundException(
+        `Rider #${id} have already been deleted or doesn't exist`
+      );
+    return this.riderRepository.remove(rider);
   }
 }

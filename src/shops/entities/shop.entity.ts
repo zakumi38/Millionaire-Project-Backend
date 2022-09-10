50cafea5d20b583f4deb03160ff48d35becfde3b
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-09-10 20:56:56 +0630

    Added: Shops' entities

--- src/shops/entities/shop.entity.ts
+++ src/shops/entities/shop.entity.ts
@@ -1,2 +1,13 @@
+import { Column, Entity } from "typeorm";
+
+@Entity()
 export class Shop {
+    @Column("varchar")
+    name: string;
+
+    @Column("varchar")
+    location: string;
+
+    @Column("varchar")
+    latitudeLongitude: string;
 }
--- src/shops/shops.module.ts
+++ src/shops/shops.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
 import { ShopsService } from "./shops.service";
 import { ShopsController } from "./shops.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Shop } from "./entities/shop.entity";
 
 @Module({
-  controllers: [ShopsController],
-  providers: [ShopsService]
+    controllers: [ShopsController],
+    providers: [ShopsService],
+    imports: [TypeOrmModule.forFeature([Shop])]
 })
 export class ShopsModule {
 }

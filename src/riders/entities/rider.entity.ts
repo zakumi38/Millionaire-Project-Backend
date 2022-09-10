68d963b5f44dae50f8fc5baa9b75e1ae510746d1
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-09-10 18:39:52 +0630

    Added: Entity for riders

--- src/riders/entities/rider.entity.ts
+++ src/riders/entities/rider.entity.ts
@@ -1,2 +1,26 @@
-export class Rider {
+import { Column, Entity } from "typeorm";
+import { Common } from "../../common/entities/common.entity";
+
+@Entity()
+export class Rider extends Common {
+  @Column("varchar")
+  name: string;
+
+  @Column("varchar", { nullable: true, unique: true })
+  email: string;
+
+  @Column("varchar", { unique: true })
+  phoneNumber: string;
+
+  @Column("float", { nullable: true })
+  overallTotalPayment: number;
+
+  @Column("float", { nullable: true })
+  todayIncome: number;
+
+  @Column("json", { nullable: true })
+  previousPayments: {
+    date: string,
+    amount: number
+  };
 }
--- src/riders/riders.module.ts
+++ src/riders/riders.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
 import { RidersService } from "./riders.service";
 import { RidersController } from "./riders.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Rider } from "./entities/rider.entity";
 
 @Module({
   controllers: [RidersController],
-  providers: [RidersService]
+  providers: [RidersService],
+  imports: [TypeOrmModule.forFeature([Rider])]
 })
 export class RidersModule {
 }

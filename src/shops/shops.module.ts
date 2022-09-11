fefcfd190ca34b454ac262944bb9c021503fd92c
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-09-11 14:00:44 +0630

    Added: CRUD for Shops

--- src/shops/shops.module.ts
+++ src/shops/shops.module.ts
@@ -1,13 +1,12 @@
-import { Module } from "@nestjs/common";
-import { ShopsService } from "./shops.service";
-import { ShopsController } from "./shops.controller";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Shop } from "./entities/shop.entity";
+import { Module } from "@nestjs/common"
+import { ShopsService } from "./shops.service"
+import { ShopsController } from "./shops.controller"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { Shop } from "./entities/shop.entity"
 
 @Module({
     controllers: [ShopsController],
     providers: [ShopsService],
-    imports: [TypeOrmModule.forFeature([Shop])]
+    imports: [TypeOrmModule.forFeature([Shop])],
 })
-export class ShopsModule {
-}
+export class ShopsModule {}

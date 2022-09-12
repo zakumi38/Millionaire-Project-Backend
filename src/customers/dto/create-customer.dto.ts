4c1c1067592248b36d1b496c31d47ee48792c211
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-09-12 21:13:35 +0630

    Added: CRUD for customers

--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,1 +1,20 @@
-export class CreateCustomerDto {}
+import { Order } from "../../orders/entities/order.entity"
+
+interface Coordinate {
+    latitude: string
+    longitude: string
+}
+
+export class CreateCustomerDto {
+    readonly name: string
+
+    readonly phoneNumber: string
+
+    readonly email?: string
+
+    readonly location?: string
+
+    readonly coordinate?: Coordinate
+
+    readonly orders?: Order[]
+}
